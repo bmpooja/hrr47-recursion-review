86524cb514d657d4bfa2e1f2447c7b8c56e4ccee
bmpooja <bmpooja@gmail.com>
2020-06-29 12:07:17 -0700

    worked on getElementbClass and StringifyJson

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,27 @@
+
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function (className
 ) {
   // your code here
+  var elementContainingClass = []
+  // variable to store result
+  var findElementContaingClass = function (element) {
+    // check if the classList of the current element contains the className
+    if (element.classList && element.classList.contains(className)) {
+      elementContainingClass.push(element)
+    }
+    if (element.childNodes) {
+      // If there are childNodes, loop through them, calling findElementContainingClass on each child
+      for (var i = 0; i < element.childNodes.length; ++i) {
+        findElementContaingClass(element.childNodes[i]);
+      }
+    }
+  }
+  findElementContaingClass(document.body);
+  return elementContainingClass;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,47 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  if (typeof obj === 'boolean') {
+    return obj ? 'true' : 'false';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'object') {
+    if (_.isEmpty(obj) && !Array.isArray(obj)) {
+      return '{}';
+    } else if (Array.isArray(obj)) {
+      var result = "["
+      for (var i = 0; i < obj.length; ++i) {
+        result += stringifyJSON(obj[i]);
+        if (i < obj.length - 1) {
+          result += ',';
+        }
+      }
+      console.log(result)
+      return result + ']';
+    } else {
+      var result = '{';
+      for (var prop in obj) {
+        if (typeof obj[prop] === 'function' || typeof obj[prop] === 'undefined') {
+          continue;
+        }
+        result += stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]) + ',';
+      }
+      console.log('result:' + result);
+      if (result[result.length - 1] === ',') {
+        result = result.substring(0, result.length - 1);
+      }
+
+      return result + '}';
+    }
+  }
 };
